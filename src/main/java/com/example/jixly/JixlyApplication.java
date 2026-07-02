e077f3b4064b2b53312bfed323c37c4140f72794
Arseny <gamzinn.201005@gmail.com>
2026-07-02 16:22:00 +0500

    first commit

---
+++ src/main/java/com/example/jixly/JixlyApplication.java
@@ -0,0 +1,13 @@
+package com.example.jixly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JixlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JixlyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/jixly/JixlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jixly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JixlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
